c6e8c87246316aa83f007f31491e8e74c0ce8047
Anxo Armada <anxoarmada@MacBook-Pro-de-Anxo.local>
2017-05-13 21:23:49 +0200

    desastre

--- Frontend/DropChain/DropChain/views.py
+++ Frontend/DropChain/DropChain/views.py
@@ -14,6 +14,12 @@ def crowdfunding_listing(request):
 def ranking_listing(request):
     return render_to_response('ranking_listing.html')
 
+def challenge_detail(request):
+    return render_to_response('challenge_detail.html')
+
+def challenge_listing_list(request):
+    return render_to_response('challenge_listing_list.html')
+
 
 def admin_index(request):
     return render_to_response('AdminIndex.html')
